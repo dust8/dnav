a35cb48ee7ed1619d696f5d1a11db9c41338d152
dust8 <dust8@users.noreply.github.com>
2020-06-25 11:19:58 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,57 @@
+let sites = [
+  {
+    id: "dev",
+    title: "开发",
+    childrens: [
+      {
+        name: "explainshell",
+        logo: "./static/img/shell.png",
+        slogan: "match command-line arguments to their help text",
+        link: "//www.explainshell.com",
+      },
+      {
+        name: "Linux命令搜索引擎",
+        logo: "",
+        slogan:
+          "最专业的Linux命令大全，内容包含Linux命令手册、详解、学习，值得收藏的Linux命令速查手册",
+        link: "//wangchujiang.com/linux-command/",
+      },
+      {
+        name: "htmlreferencel",
+        logo: "./static/img/html-reference-icon.png",
+        slogan: "A free guide to all HTML elements and attributes",
+        link: "//htmlreference.io",
+      },
+      {
+        name: "cssreferencel",
+        logo: "./static/img/css-reference-icon.png",
+        slogan: "CSS Reference is a free visual guide to CSS",
+        link: "//cssreference.io",
+      },
+      {
+        name: "GitHub 文件加速",
+        logo: "./static/img/workers.png",
+        slogan: "github release、archive以及项目文件的加速项目",
+        link: "//shrill-pond-3e81.hunsh.workers.dev",
+      },
+    ],
+  },
+  {
+    id: "yule",
+    title: "娱乐",
+    childrens: [
+      {
+        name: "novipnoad",
+        logo: "./static/img/novipnoad.png",
+        slogan: "NO视频 不负追剧好时光 (￣▽￣)",
+        link: "//www.novipnoad.com/",
+      },
+      {
+        name: "博海拾贝",
+        logo: "./static/img/bh.sb.ico",
+        slogan: "一个有趣的网站",
+        link: "//bh.sb/post/category/main/",
+      },
+    ],
+  },
+];
